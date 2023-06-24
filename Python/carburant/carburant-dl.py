fd964c8dd63bfa5fd7f4223699fddf476fe72904
awadesu <133776560+awadesu@users.noreply.github.com>
2023-06-24 23:45:37 +0200

    fix working directory

--- Python/carburant/carburant-dl.py
+++ Python/carburant/carburant-dl.py
@@ -1,7 +1,7 @@
 import requests
-
+import os
+os.chdir("/home/awakeneru/repos/file-transfer/Python/carburant")
 LIENinstant = "https://www.data.gouv.fr/fr/datasets/r/edd67f5b-46d0-4663-9de9-e5db1c880160"
 r = requests.get(LIENinstant)
-
-open('carburant/prix-des-carburants-en-france-flux-instantane-v2.csv',
+open('prix-des-carburants-en-france-flux-instantane-v2.csv',
      'wb').write(r.content)
